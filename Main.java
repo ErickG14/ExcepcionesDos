b942db2070e569cd389728fd4f057bdf505a432c
ErickG14 <124231061+ErickG14@users.noreply.github.com>
2023-05-04 23:15:01 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        LeerArchivo lectura = new LeerArchivo();
+        try {
+            lectura.metodouno("datos.txt");
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+}
